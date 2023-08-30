523758a2b68777baaf4d5a36e3db756c427beea4
Ubikuiity <pathierfabien@gmail.com>
2023-08-31 00:02:59 +0200

    Advancing on the detecting of links in files...

--- watchAndChange.ts
+++ watchAndChange.ts
@@ -1,10 +1,32 @@
 import Admonition144 from "main";
 import { watch, FSWatcher, link } from "fs";
+import { TFile } from "obsidian";
+
+// This interface is needed to represent a link in a file
+class fileLink {
+    pointedFile: TFile;  // The file being linked
+    linkString: string;  // The string that contains the link to the file
+    originalFile: TFile;  // The file containing the link
+
+    /**
+     * Class used to contruct the link object
+     * 
+     * @param pFile file being pointed by the link
+     * @param linkString string of the link [[XXXXX]]
+     * @param oFile file containing the link
+     */
+     constructor(pFile: TFile, linkString: string, oFile: TFile) {
+        this.pointedFile = pFile;
+        this.linkString = linkString;
+        this.originalFile = oFile;
+    }
+}
 
 export class fileWatcher {
     mainPlugin: Admonition144;
     watcher: FSWatcher;
     fileNameBuffer: customNameBuffer;
+    linksFinder: formerLinksReplacer;
 
     /**
     * Class used to contruct the fileWatcher and call subclasses
@@ -13,7 +35,8 @@ export class fileWatcher {
     */
     constructor(plugin: Admonition144) {
         this.mainPlugin = plugin;
-        this.fileNameBuffer = new customNameBuffer(plugin);
+        this.linksFinder = new formerLinksReplacer(plugin);
+        this.fileNameBuffer = new customNameBuffer(this);
     }
 
     monitorChanges() {
@@ -37,32 +60,67 @@ export class fileWatcher {
 }
 
 class customNameBuffer {
-    mainPlugin: Admonition144;
+    fWatcher: fileWatcher;
     nameBuffer: Array<string>;  // Variable where we append the old and new names
 
     /**
-    * Class used to contruct the news file.
+    * Class used to contruct the name buffer.
+    * 
+    * @param fWatcher main watcher of files changes
+    */
+    constructor(fWatcher: fileWatcher) {
+        this.fWatcher = fWatcher;
+        this.nameBuffer = new Array<string>;
+    }
+
+    addChangeName(fileName: string) {
+        this.nameBuffer.push(fileName);
+        if (this.nameBuffer.length >= 2) {
+            let oldName: string = this.nameBuffer[0];
+            let newName: string = this.nameBuffer[1];
+            console.log(`Detected renaming of file ${oldName} to ${newName}`);
+            this.fWatcher.linksFinder.replaceLinks(oldName, newName);
+        }
+    }
+}
+
+class formerLinksReplacer {
+    mainPlugin: Admonition144;
+    /**
+    * Class used to detect links between files
     * 
     * @param plugin Main plugin
     */
     constructor(plugin: Admonition144) {
         this.mainPlugin = plugin;
-        this.nameBuffer = new Array<string>;
     }
+    
+    // Get list of admonition links in all files
+    async replaceLinks(oldName: string, newName: string) {
+        // Go through each file to check if there is a link to the file that has been renamed
+        const files = this.mainPlugin.app.vault.getMarkdownFiles();
 
-    addChangeName(fileName: string){
-        this.nameBuffer.push(fileName);
-        if (this.nameBuffer.length >= 2) {
-            let oldName: string = this.nameBuffer[0];
-            let newName: string = this.nameBuffer[1];
-            console.log(`Detected renaming of file ${oldName} to ${newName}`)
+        for (let mdFile of files) {
+            const content: string = await this.mainPlugin.app.vault.cachedRead(mdFile);
+            const minimalLink: string = getMinimumLinkFromFileName(oldName)
+
+            // First check to avoid further inspection of most of files
+            if (content.includes(minimalLink)) {
+                // Now, let's look at all admonition codeblocks, we will get them via a regex
+                const reAdmonitionCodeblocks = new RegExp(`\\\`{3} *ad[^\\\`]*\\\`{3}`, "g");
+            }
         }
     }
 }
 
-// Now we have to scan all vault and look for links containing file name. then change these links. Maybe ask the user for confirmation
-
-function getMinimumLinkFromFileName(fileName: string): string{
+/**
+* Function that returns the minimal string to link a file given the full path of the file
+*
+* For example : Test/TestFolder\MyTestLink.md -> MyTestLink
+* 
+* @param fileName relative path of file (from vault root)
+*/
+function getMinimumLinkFromFileName(fileName: string): string {
     let res: string = fileName;
     res = res.split('/').pop();
     // Test/TestFolder\MyTestLink.md -> TestFolder\MyTestLink.md
